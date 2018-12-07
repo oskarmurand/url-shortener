48f8aeee31608526fbc1cfa968c0c7757b078018
Oskar Murand <oskar@testlio.com>
2018-12-07 16:58:04 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,70 @@
+'use static';
+
+const { json, createError, sendError, send } = require('micro');
+const { parse } = require('url');
+const storage = require('node-persist');
+
+const redirect = async(res, url) => {
+    res.statusCode = 301;
+    res.setHeader('Location', await url);
+    res.end();
+}
+
+const post = async(req, res) => {
+    const {url, alias } = await json(req);    
+    if (await read(alias)) {
+        send(res, 400, 'alias already exists');
+        handle
+    };
+
+    try {
+        await write(url, alias);
+    } catch(e) {
+        return createError(500);
+    }
+
+    return({url});
+};
+
+const read = async(key) => {
+    try {
+        await storage.init();
+        const { url } = await storage.getItem(key)
+        return url;
+    } catch(e) {
+        return false;
+    }
+}
+
+const write = async(url, alias) => {
+    await storage.init();
+    return await storage.setItem(alias, {
+        'url': url
+    });
+}
+
+const get = async (req, res) => {
+    parsedReq = parse(req.url, true);
+    const alias = parsedReq.pathname.slice(1);
+    const url = await read(alias);
+
+    if (!url) return send(res, 404, 'not found');
+    if (parsedReq.query.noRedirect) return url;
+    return redirect(res, url)
+};
+
+module.exports = async (req, res) => {
+    try {
+        switch (req.method) {
+        case 'POST':
+          return await post(req, res);
+        case 'GET':
+          return await get(req, res);
+        default:
+          send(res, 405, 'Invalid method');
+          break;
+        }
+      } catch (error) {
+        throw error;
+      }
+  }
